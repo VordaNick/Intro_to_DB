a0d1d480bec18cb12aded73bbb950bafeff0ab0f
VordaNick <vordanick@gmail.com>
2024-10-23 12:10:22 -0700

    Added

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,5 +1,5 @@
 /* comment */
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 CREATE TABLE Books ( book_id PRIMARY KEY, title VARCHAR(130), author_id FOREIGN KEY (Authors), price DOUBLE, publication_date DATABASE);
 CREATE TABLE Authors ( author_id PRIMARY KEY, author_name VARCHAR(215))
