3150f3fb85ad44ad74221d76a6e6d6576023ac1d
VordaNick <vordanick@gmail.com>
2024-10-23 12:16:34 -0700

    Added

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -4,5 +4,5 @@ USE alx_book_store;
 CREATE TABLE Books ( book_id PRIMARY KEY, title VARCHAR(130), author_id FOREIGN KEY (Authors), price DOUBLE, publication_date DATABASE);
 CREATE TABLE Authors ( author_id PRIMARY KEY, author_name VARCHAR(215))
 CREATE TABLE Customers (customer_id PRIMARY KEY, customer_name VARCHAR(215), email VARCHAR(215), address TEXT)
-CREATE TABLE Orders ( order_id PRIMARY KEY, customer_id FOREIGN KEY(Customers), order_date DATE)
+CREATE TABLE Orders ( order_id INT, customer_id INT, FOREIGN KEY(Customer_id) REFERENCES Customers(customer_id))
 CREATE TABLE Order_Details ( order_detail_id PRIMARY KEY, order_id FOREIGN KEY(Orders), book_id FOREIGN KEY(Books TABLE), quantity DOUBLE)
